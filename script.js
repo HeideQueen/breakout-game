9e96953b5dee835d7baec39ce68323dd3217340b
HeideQueen <lucyfabz@gmail.com>
2020-07-10 01:44:40 -0500

    move ball & break bricks

--- script.js
+++ script.js
@@ -102,6 +102,48 @@ function movePaddle() {
   }
 }
 
+// move ball
+function moveBall() {
+  ball.x += ball.dx;
+  ball.y += ball.dy;
+
+  // wall detection (right left)
+  if (ball.x + ball.size > canvas.width || ball.x - ball.size < 0) {
+    ball.dx *= -1;
+  }
+
+  // wall detection (top bottom)
+  if (ball.y + ball.size > canvas.height || ball.y - ball.size < 0) {
+    ball.dy *= -1;
+  }
+
+  // paddle detection
+  if (
+    ball.x - ball.size > paddle.x &&
+    ball.x + ball.size < paddle.x + paddle.w &&
+    ball.y + ball.size > paddle.y
+  ) {
+    ball.dy = -ball.speed;
+  }
+
+  // brick collision
+  bricks.forEach((column) => {
+    column.forEach((brick) => {
+      if (brick.visible) {
+        if (
+          ball.x - ball.size > brick.x &&
+          ball.x + ball.size < brick.x + brick.w &&
+          ball.y + ball.size > brick.y &&
+          ball.y - ball.size < brick.y + brick.h
+        ) {
+          ball.dy *= -1;
+          brick.visible = false;
+        }
+      }
+    });
+  });
+}
+
 // draw everyhing
 function draw() {
   // clear canvas
@@ -116,6 +158,7 @@ function draw() {
 // update canvas drawing
 function update() {
   movePaddle();
+  moveBall();
   // draw everything
   draw();
 
